18c575f97f8d34e9ee9601b53ef7d3ba56b081a0
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-01-23 17:33:47 +0100

    Refactor

--- src/nomad_simulation_parsers/parsers/__init__.py
+++ src/nomad_simulation_parsers/parsers/__init__.py
@@ -26,7 +26,6 @@ class EntryPoint(ParserEntryPoint):
                 f'Could not load parser class {self.parser_class_name}', exc_info=e
             )
 
-
 class Wannier90ParserEntryPoint(EntryPoint):
     equal_cell_positions_tolerance: float = Field(
         1e-2,
