3ff6bb8207de2f23ca6998ef98ac1fb5823839bf
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-04-09 14:10:19 +0200

    Add phonopy test

---
+++ tests/parsers/test_phonopy_parser.py
@@ -0,0 +1,11 @@
+from nomad.datamodel import EntryArchive
+from nomad.utils import get_logger
+
+from nomad_simulation_parsers.parsers.phonopy.parser import PhonopyParser
+
+LOGGER = get_logger(__name__)
+
+def test_parse_file():
+    parser = PhonopyParser()
+    archive = EntryArchive()
+    parser.parse('tests/data/phonopy/vasp/phonopy.yaml', archive, LOGGER)
