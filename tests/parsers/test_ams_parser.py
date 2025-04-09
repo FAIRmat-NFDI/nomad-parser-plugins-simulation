d24a7350dedb0c3f8d4fd17f0583fdc6fc1cd1b7
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2025-04-09 21:09:18 +0200

    Add ams test

---
+++ tests/parsers/test_ams_parser.py
@@ -0,0 +1,11 @@
+from nomad.datamodel import EntryArchive
+from nomad.utils import get_logger
+
+from nomad_simulation_parsers.parsers.ams.parser import AMSParser
+
+LOGGER = get_logger(__name__)
+
+def test_parse_file():
+    parser = AMSParser()
+    archive = EntryArchive()
+    parser.parse('tests/data/ams/scf/phenylrSmall-metagga.out', archive, LOGGER)
