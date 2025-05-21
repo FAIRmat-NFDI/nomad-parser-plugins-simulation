62ec00bc8f1f4382f2bf4c5d5c88f06d70fc7a5d
ladinesa <67910858+ladinesa@users.noreply.github.com>
2025-05-21 11:09:26 +0200

    Extend parsers (#19)
    * Add system quantities to qe
    
    * Add output quantities to qe
    
    * Extend octopus parser
    
    * Extend ams parser

--- src/nomad_simulation_parsers/parsers/ams/parser.py
+++ src/nomad_simulation_parsers/parsers/ams/parser.py
@@ -1,5 +1,8 @@
+import os
 from importlib import reload
+from typing import Any
 
+import numpy as np
 from nomad.datamodel import EntryArchive
 from nomad.parsing.file_parser import ArchiveWriter
 from nomad.parsing.file_parser.mapping_parser import MetainfoParser, TextParser
@@ -8,14 +11,54 @@ from nomad.utils import get_logger
 from nomad_simulations.schema_packages.general import Program, Simulation
 from structlog.stdlib import BoundLogger
 
+from nomad_simulation_parsers.parsers.utils.general import search_files
 from nomad_simulation_parsers.schema_packages import ams
 
 from .file_parser import OutParser
+from .file_parser import RKFParser as RKFTextParser
 
 LOGGER = get_logger(__name__)
 
 
 class MainfileParser(TextParser):
+    # TODO temporary fix for structlog unable to propagate logger
+    @property
+    def logger(self):
+        return LOGGER
+
+    def get_xc_functionals(self, source: dict[str, Any]) -> list[str]:
+        xc_functionals = []
+        for xc_type in ['LDA', 'GGA', 'MGGA']:
+            functionals = source.get(xc_type, '').split()
+            kind = ['XC'] if len(functionals) == 1 else ['X', 'C']
+            for n, functional in enumerate(functionals):
+                xc_functionals.append(
+                    f'{xc_type}_{kind[n]}_{functional.rstrip("x").rstrip("c").upper()}'
+                )
+        return xc_functionals
+
+    def get_contributions(self, source: dict[str, Any]) -> list[dict[str, Any]]:
+        return [
+            dict(name=key, value=val) for key, val in source.items() if val is not None
+        ]
+
+    def get_eigenvalues(
+        self, source: dict[str, Any] | list[np.ndarray]
+    ) -> list[dict[str, np.ndarray]]:
+        energies = source.get('energies', []) if isinstance(source, dict) else []
+        occupations = (
+            source.get('occupations', []) if isinstance(source, dict) else source[2]
+        )
+        nspin = max(len(energies), len(occupations))
+        eigenvalues = [dict() for _ in range(nspin)]
+        for n, energy in enumerate(energies):
+            eigenvalues[n]['eigenvalues'] = energy
+        for n, occupations in enumerate(occupations):
+            eigenvalues[n]['occupations'] = occupations
+        return [eig for eig in eigenvalues if eig]
+
+
+class RKFParser(MainfileParser):
     # TODO temporary fix for structlog unable to propagate logger
     @property
     def logger(self):
@@ -32,6 +75,7 @@ class AMSMetainfoParser(MetainfoParser):
 class AMSArchiveWriter(ArchiveWriter):
     mainfile_parser = MainfileParser(text_parser=OutParser())
     metainfo_parser = AMSMetainfoParser()
+    rkf_parser = RKFParser(text_parser=RKFTextParser())
 
     def write_to_archive(self):
         # reload schema package to use correct annotations
@@ -41,8 +85,17 @@ class AMSArchiveWriter(ArchiveWriter):
         self.archive.data = Simulation(program=Program(name='AMS'))
         self.metainfo_parser.data_object = self.archive.data
 
-        self.mainfile_parser.filepath = self.mainfile
-        self.mainfile_parser.convert(self.metainfo_parser)
+        rkf_files = search_files('ams.rkf', os.path.dirname(self.mainfile))
+        self.parser = self.mainfile_parser
+        self.parser.filepath = self.mainfile
+        if rkf_files:
+            if len(rkf_files) > 1:
+                self.logger.warning('Multiple ams.rkf files found.')
+            self.parser = self.rkf_parser
+            self.parser.filepath = rkf_files[0]
+            self.parser.data_object.parse()
+
+        self.parser.convert(self.metainfo_parser)
 
 
 class AMSParser(MatchingParser):
--- src/nomad_simulation_parsers/parsers/quantumespresso/pwscf/parser.py
+++ src/nomad_simulation_parsers/parsers/quantumespresso/pwscf/parser.py
@@ -1,4 +1,8 @@
+from typing import Any
+
+import numpy as np
 from nomad.datamodel import EntryArchive
+from nomad.units import ureg
 from nomad.utils import get_logger
 
 from nomad_simulation_parsers.parsers.quantumespresso.parser import (
@@ -17,6 +21,49 @@ class PWSCFMainfileParser(MainfileParser):
     @property
     def logger(self):
         return LOGGER
+
+    def get_force_contributions(self, source: dict[str, Any]) -> list[dict[str, Any]]:
+        keys = ['dispersion']
+        return [
+            dict(name=key, value=source[f'forces_{key}'])
+            for key in keys
+            if source.get(f'forces_{key}' is not None)
+        ]
+
+    def get_eigenvalues(self, source: dict[str, Any]) -> list[dict[str, Any]]:
+        eigenvalues = source.get('band_energies')
+        if eigenvalues is None:
+            return []
+        n_spin = self.get_n_spin_channels()
+        n_eigs = len(eigenvalues[0])
+        n_bands = np.size(eigenvalues) // int(n_spin * n_eigs)
+        eigenvalues = np.reshape(eigenvalues, (n_spin, n_bands, n_eigs)) * ureg.eV
+        results = [dict(eigenvalues=eig) for n, eig in enumerate(eigenvalues)]
+        occupations = source.get('occupation_numbers')
+        if occupations is not None:
+            occupations = np.reshape(occupations, (n_spin, n_bands, n_eigs))
+            for n, occ in enumerate(occupations):
+                results[n]['occupations'] = occ
+        return results
+
+    def get_configurations(self, source: dict[str, Any]) -> list[dict[str, Any]]:
+        methods = {
+            'self_consistent': 'single_point',
+            'bandstructure': 'single_point',
+            'bfgs_geometry_optimization': 'geometry_optimization',
+            'molecular_dynamics': 'molecular_dynamics',
+            'langevin_dynamics': 'langevin_dynamics',
+            'damped_dynamics': 'geometry_optimization',
+            'vcs_wentzcovitch_damped_minimization': 'geometry_optimization',
+        }
+
+        configurations = []
+        for key in methods:
+            config = source.get(key)
+            if config is None:
+                continue
+            configurations.append(config.get('self_consistent', config))
+        return configurations
 
 
 class PWSCFArchiveWriter(QuantumEspressoArchiveWriter):
