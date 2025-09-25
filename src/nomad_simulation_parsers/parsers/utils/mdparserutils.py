e7216881e9eb1c4a51eeb398051ebf526e382de0
Bernadette Mohr <bernadette.mohr@physik.hu-berlin.de>
2025-09-25 15:15:48 +0200

    Removed print statements, changed workflow schema to nomad_simulations

--- src/nomad_simulation_parsers/parsers/utils/mdparserutils.py
+++ src/nomad_simulation_parsers/parsers/utils/mdparserutils.py
@@ -25,6 +25,9 @@ import numpy as np
 # from nomad.metainfo import MSection
 from nomad.parsing.file_parser import ArchiveWriter
 from nomad.utils import get_logger
+
+# from runschema.method import Interaction, Model
+from nomad_simulations.schema_packages import workflow
 from nomad_simulations.schema_packages.atoms_state import ParticleState
 from nomad_simulations.schema_packages.general import Simulation
 from nomad_simulations.schema_packages.model_system import Cell, ModelSystem
@@ -37,9 +40,6 @@ from nomad_simulations.schema_packages.outputs import (
 )
 from nomad_simulations.schema_packages.properties.energies import BaseEnergy
 from nomad_simulations.schema_packages.properties.forces import BaseForce
-
-# from runschema.method import Interaction, Model
-from simulationworkflowschema import MolecularDynamics
 
 
 class MDParser(ArchiveWriter):
@@ -227,7 +227,7 @@ class MDParser(ArchiveWriter):
         if self.archive is None:
             return
 
-        sec_workflow = MolecularDynamics()
+        sec_workflow = workflow.MolecularDynamics()
         self.parse_section(data, sec_workflow)
         self.archive.workflow2 = sec_workflow
 
