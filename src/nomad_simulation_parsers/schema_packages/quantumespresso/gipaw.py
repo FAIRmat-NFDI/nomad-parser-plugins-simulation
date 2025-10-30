c54465796f10e8326c5159076b25dfd91b265fc5
cecizag <cecilia.zagni@exact-lab.it>
2025-10-30 10:17:07 +0100

    mapped Section with add_mapping_annotations

--- src/nomad_simulation_parsers/schema_packages/quantumespresso/gipaw.py
+++ src/nomad_simulation_parsers/schema_packages/quantumespresso/gipaw.py
@@ -12,13 +12,13 @@ m_package = SchemaPackage()
 
 class MagneticShielding(schema_package.MagneticShielding):
     add_mapping_annotations(
-        schema_package.MagneticShielding.value, OUT_KEY, '.magnetic_shieldings'
+        schema_package.MagneticShielding.value, OUT_KEY, '.@'
     )
 
 
 class Outputs(schema_package.Outputs):
     add_mapping_annotations(
-        schema_package.Outputs.magnetic_shieldings,
+        schema_package.Outputs.m_def,
         OUT_KEY,
         ('get_magnetic_shieldings', ['.@']),
     )
