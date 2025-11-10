bb2859fe203a8dc9a61936e6976d79cff07cb062
cecizag <cecilia.zagni@exact-lab.it>
2025-11-10 09:34:31 +0100

    updated tests and test data

--- tests/parsers/test_quantumespresso_parser.py
+++ tests/parsers/test_quantumespresso_parser.py
@@ -14,6 +14,16 @@ def test_pwscf():
     parser.parse('tests/data/quantumespresso/pwscf/TiO2_opt/pw.out', archive, LOGGER)
 
 
+
+def test_pwscf_xml():
+    parser = QuantumEspressoParser()
+    archive = EntryArchive()
+    parser.parse('tests/data/quantumespresso/pwscf/TiO2_opt/TiO2.save/data-file-schema.xml', archive, LOGGER)
+    from devtools import debug
+    debug(archive.data.model_system)
+
+
+
 def test_epw():
     parser = QuantumEspressoParser()
     archive = EntryArchive()
@@ -35,22 +45,41 @@ def test_xspectra():
         LOGGER,
     )
 
-
-def test_gipaw_nmr():
+from devtools import debug
+def test_gipaw_nmr_text():
     parser = QuantumEspressoParser()
     archive = EntryArchive()
     parser.parse(
-        'tests/data/quantumespresso/gipaw/quartz-nmr/quartz-nmr.out',
+        'tests/data/quantumespresso/gipaw/scf_out_nmr_out_741/quartz-nmr.out',
         archive,
         LOGGER,
     )
+    debug(archive.data.outputs)
+    debug(archive.data.outputs[0])
+    debug(archive.data.outputs[0].magnetic_shieldings)
+    debug(archive.data.outputs[0].magnetic_susceptibilities)
+    
+
+def test_gipaw_nmr_xml():
+    parser = QuantumEspressoParser()
+    archive = EntryArchive()
+    parser.parse(
+        'tests/data/quantumespresso/gipaw/scf_xml_nmr_xml/quartz-nmr.xml',
+        archive,
+        LOGGER,
+    )
+    debug(archive.data.outputs)
 
 
 def test_gipaw_scf():
     parser = QuantumEspressoParser()
     archive = EntryArchive()
     parser.parse(
-        'tests/data/quantumespresso/gipaw/quartz-nmr/quartz-scf.out',
+        'tests/data/quantumespresso/gipaw/scf_out_nmr_out_741/quartz-scf.out',
         archive,
         LOGGER,
     )
+    debug(archive.data.outputs[0])
+
+
+
