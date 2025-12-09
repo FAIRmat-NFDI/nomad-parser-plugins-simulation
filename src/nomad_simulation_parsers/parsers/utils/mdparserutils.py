f2bcfe64833fbd697b34bdef4e9d6424ec875551
bernadette <bernadette.mohr@physik.hu-berlin.de>
2025-12-09 19:09:57 +0100

    Fix usage of representation schema in mdparserutils (#128)
    * Fix: Replace Representation with AlternativeRepresentation
    
    * Directly append lattice vectors and pbc to `model_szstem` in `parse_trajectory_step`
    
    * Corrected setting of `lattice_vectors` and `periodic_boundary_conditions`

--- src/nomad_simulation_parsers/parsers/utils/mdparserutils.py
+++ src/nomad_simulation_parsers/parsers/utils/mdparserutils.py
@@ -23,9 +23,14 @@ from typing import Any
 import numpy as np
 from nomad.parsing.file_parser import ArchiveWriter
 from nomad.utils import get_logger
+
+# from runschema.method import Interaction, Model
+from nomad_simulations.schema_packages import workflow
 from nomad_simulations.schema_packages.atoms_state import ParticleState
 from nomad_simulations.schema_packages.general import Simulation
-from nomad_simulations.schema_packages.model_system import ModelSystem, Representation
+from nomad_simulations.schema_packages.model_system import (
+    ModelSystem,
+)
 
 # nomad-simulations
 from nomad_simulations.schema_packages.outputs import (
@@ -37,9 +42,6 @@ from nomad_simulations.schema_packages.properties.energies import BaseEnergy
 from nomad_simulations.schema_packages.properties.forces import BaseForce
 
 # from runschema.method import Interaction, Model
-from nomad_simulations.schema_packages.workflow.molecular_dynamics import (
-    MolecularDynamics,
-)
 
 
 class MDParser(ArchiveWriter):
@@ -145,7 +147,7 @@ class MDParser(ArchiveWriter):
         data: dict[str, Any],
         simulation: Simulation,
         model_system: ModelSystem = None,
-        representation: Representation = None,
+        # representations: AlternativeRepresentation = None,
     ) -> None:
         """
         Create a system section and write the provided data.
@@ -157,16 +159,21 @@ class MDParser(ArchiveWriter):
             return
         if model_system is None:
             model_system = ModelSystem()
-        if representation is None:
-            representation = Representation()
 
-        cell_dict = data.pop('cell')
+        lattice_vectors = data.pop('lattice_vectors')
+        periodic_boundary_conditions = data.pop('periodic_boundary_conditions')
         particle_labels = data.pop('labels')
+        dimensions = data.pop('dimensions')
+
         for label in particle_labels:
             particle_state = ParticleState(label=label)
             model_system.particle_states.append(particle_state)
-        self.parse_section(cell_dict, representation)
-        model_system.representations.append(representation)
+
+        model_system.lattice_vectors = lattice_vectors
+        model_system.periodic_boundary_conditions = periodic_boundary_conditions
+
+        model_system.dimensionality = dimensions
+
         self.parse_section(data, model_system)
         simulation.model_system.append(model_system)
 
@@ -225,7 +232,7 @@ class MDParser(ArchiveWriter):
         if self.archive is None:
             return
 
-        sec_workflow = MolecularDynamics()
+        sec_workflow = workflow.MolecularDynamics()
         self.parse_section(data, sec_workflow)
         self.archive.workflow2 = sec_workflow
 
