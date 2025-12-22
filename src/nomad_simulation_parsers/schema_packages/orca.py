bafd2cce55789a49e6a98e49da9efedef5a8b953
EBB2675 <esmaboydas4@gmail.com>
2025-12-22 15:32:07 +0100

    fix lattice_vectors issue

--- src/nomad_simulation_parsers/schema_packages/orca.py
+++ src/nomad_simulation_parsers/schema_packages/orca.py
@@ -72,6 +72,92 @@ add_mapping_annotation(
     '.global_exact_exchange',
 )
 
+############# Multireference (CAS) ###################
+
+# add_mapping_annotation(
+#     model_method.MultireferenceSCF.m_def,
+#     OUT_KEY,
+#     ('get_multireference', ['.@'], dict(target='scf')),
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferenceCI.m_def,
+#     OUT_KEY,
+#     ('get_multireference', ['.@'], dict(target='ci')),
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferencePT.m_def,
+#     OUT_KEY,
+#     ('get_multireference', ['.@'], dict(target='pt')),
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferenceSCF.type,
+#     OUT_KEY,
+#     '.type',
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferenceCI.type,
+#     OUT_KEY,
+#     '.type',
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferencePT.type,
+#     OUT_KEY,
+#     '.type',
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferencePT.order,
+#     OUT_KEY,
+#     '.order',
+# )
+
+# add_mapping_annotation(
+#     model_method.BaseMultireferenceMethod.reference_type,
+#     OUT_KEY,
+#     '.reference_type',
+# )
+
+# add_mapping_annotation(
+#     model_method.BaseMultireferenceMethod.n_state_groups,
+#     OUT_KEY,
+#     '.n_state_groups',
+# )
+
+# add_mapping_annotation(
+#     model_method.BaseMultireferenceMethod.state_multiplicities,
+#     OUT_KEY,
+#     '.state_multiplicities',
+# )
+
+# add_mapping_annotation(
+#     model_method.BaseMultireferenceMethod.n_roots_per_multiplicity,
+#     OUT_KEY,
+#     '.n_roots_per_multiplicity',
+# )
+
+# add_mapping_annotation(
+#     model_method.MultireferenceSCF.active_space,
+#     OUT_KEY,
+#     '.active_space',
+# )
+
+# add_mapping_annotation(
+#     model_method.ActiveSpace.n_active_electrons,
+#     OUT_KEY,
+#     '.active_space.n_active_electrons',
+# )
+
+# add_mapping_annotation(
+#     model_method.ActiveSpace.n_active_orbitals,
+#     OUT_KEY,
+#     '.active_space.n_active_orbitals',
+# )
+
 
 try:
     m_package.__init_metainfo__()
