371530c6c022ad02986d6a630cb744ce345b80f3
Amir Golparvar <golparvar.amir@gmail.com>
2026-02-04 10:16:28 +0100

    Fix linting and formatting (imports)

--- src/nomad_simulation_parsers/parsers/h5md/parser.py
+++ src/nomad_simulation_parsers/parsers/h5md/parser.py
@@ -8,14 +8,10 @@ from nomad.parsing.file_parser.mapping_parser import HDF5Parser, MetainfoParser,
 from nomad.parsing.parser import MatchingParser
 from nomad.units import ureg
 from nomad.utils import get_logger
-
-# from nomad_simulations.schema_packages.workflow import molecular_dynamics
 from structlog.stdlib import BoundLogger
 
 from nomad_simulation_parsers.parsers.utils.mdparserutils import MDParser
 from nomad_simulation_parsers.schema_packages import h5md
-
-# from nomad_simulation_parsers.schema_packages.h5md import MolecularDynamics, Simulation
 from nomad_simulation_parsers.schema_packages.utils import remove_mapping_annotations
 
 LOGGER = get_logger(__name__)
