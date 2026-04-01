adb05970831a90f2aae6cfd35f633d0236599f80
Alvin Noe Ladines <ladinesalvinnoe@gmail.com>
2026-04-01 12:54:22 +0200

    Execute legacy first

--- src/nomad_simulation_parsers/parsers/crystal/parser.py
+++ src/nomad_simulation_parsers/parsers/crystal/parser.py
@@ -369,10 +369,10 @@ class CrystalParser(MatchingParser):
         logger: BoundLogger,
         child_arhives: dict[str, EntryArchive] = {},
     ):
-        self.archive_writer.write(mainfile, archive, logger, child_arhives)
 
         # run the old parser
         # TODO remove
         from electronicparsers.crystal.parser import CrystalParser  # noqa
 
         CrystalParser().parse(mainfile, archive, logger)
+        self.archive_writer.write(mainfile, archive, logger, child_arhives)
