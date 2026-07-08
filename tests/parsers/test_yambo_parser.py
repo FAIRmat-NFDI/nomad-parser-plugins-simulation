d38bd2df15966c27e79c7e70d7c5b0f4e3e960bb
Elena Molteni <65666317+emolteni@users.noreply.github.com>
2026-07-08 15:01:57 +0200

    Create test_yambo_parser.py for testing yambo spectra parsing

---
+++ tests/parsers/test_yambo_parser.py
@@ -0,0 +1,14 @@
+from nomad.datamodel import EntryArchive
+from nomad.utils import get_logger
+
+from nomad_simulation_parsers.parsers.yambo  import YamboArchiveWriter   
+
+LOGGER = get_logger(__name__)
+
+def test_spectra(parser):
+  archive = EntryArchive()
+  parser = YamboArchiveWriter()
+  parser.parse('tests/data/yambo/r_setup',archive,LOGGER)
+  assert spectra_files[0] = 'o-R_methylox_TDLDA.alpha_q1_slepc_alda_bse'
+  assert sp_type = 'Polarizability'
+  assert spectra_obj.shape = (4000,2)
